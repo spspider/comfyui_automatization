ec438a7115e773a8567615f84fb8934ef4bcdf33
serhii paukov <spspider95@gmail.com>
2025-08-07 12:26:54 +0300

    wan2.2vace

--- workflow_run/run_t2v_wan22.py
+++ workflow_run/run_t2v_wan22.py
@@ -4,19 +4,21 @@ import os
 import time
 from pathlib import Path
 import shutil
+from datetime import datetime
 
-def run_text2video(prompt, negative_prompt =None, workflow_path="workflows/want2v_2.2_api_av1.json", video_seconds=5):
+def run_text2video(prompt, negative_prompt =None, workflow_path="workflows/want2v_2.2_api_mp4_vace.json", video_seconds=5):
     COMFY_URL = "http://127.0.0.1:8188"
     OUTPUT_DIR = Path("c:/AI/ComfyUI_windows_portable/ComfyUI/output/")
     RESULT_DIR = Path("c:/AI/comfyui_automatization/result/")
     RESULT_DIR.mkdir(exist_ok=True)
     
-    FRAME_RATE = 16  # Assuming that the default frame rate is still a valid parameter
+    FRAME_RATE = 24  # Assuming that the default frame rate is still a valid parameter
     FRAME_COUNT = video_seconds * FRAME_RATE
     # Load API-exported workflow
     with open(workflow_path, "r", encoding="utf-8") as f:
         workflow = json.load(f)
-
+        
+    filename_prefix = workflow["57"]["inputs"].get("filename_prefix", "wan")
     # Set positive and negative prompts
     if "6" in workflow:
         workflow["6"]["inputs"]["text"] = prompt
@@ -35,7 +37,9 @@ def run_text2video(prompt, negative_prompt =None, workflow_path="workflows/want2
     print(f"✅ Prompt submitted. ID: {prompt_id}")
 
     # Wait for completion
-    print("⌛ Waiting for completion...")
+    from datetime import datetime
+    timestamp = datetime.now().strftime("%H:%M")
+    print(f"⌛ Waiting for completion... [{timestamp}]")
     while True:
         r = requests.get(f"{COMFY_URL}/history/{prompt_id}")
         if r.status_code == 200:
@@ -46,7 +50,7 @@ def run_text2video(prompt, negative_prompt =None, workflow_path="workflows/want2
         time.sleep(1)
 
     # Find latest .webm output
-    files = list(OUTPUT_DIR.glob(f"{filename_prefix}_*.webm"))
+    files = list(OUTPUT_DIR.glob(f"{filename_prefix}_*.mp4"))
     if not files:
         print("❌ No .webm output found.")
         return None
